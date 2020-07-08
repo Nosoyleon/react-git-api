1481352c5b0f28a2af1f8020e4e952eceba93fea
Leon Ceballos <leonceballos@Erre-MacBook-Pro-10.local>
2020-07-07 22:58:08 -0500

    Fix conditional rendering

--- src/app/components/GeneralTable/index.js
+++ src/app/components/GeneralTable/index.js
@@ -10,81 +10,81 @@ function GeneralTable({ headers, data, errorMessage, loading, page, setPage }) {
 
   return (
     <>
-      <table className="table is-striped is-hoverable is-fullwidth">
-        <thead>
-          <tr>
-            {headers.map(({ accesor, label }) => (
-              <th key={accesor}>{label}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, rowIndex) => (
-            <tr key={rowIndex.toString()}>
-              {headers.map(({ accesor }, celIndex) => (
-                <td key={celIndex.toString()}>{row[accesor]}</td>
+      {!loading && (
+        <>
+          <table className="table is-striped is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                {headers.map(({ accesor, label }) => (
+                  <th key={accesor}>{label}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((row, rowIndex) => (
+                <tr key={rowIndex.toString()}>
+                  {headers.map(({ accesor }, celIndex) => (
+                    <td key={celIndex.toString()}>{row[accesor]}</td>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <nav
-        className="pagination is-rounded mb-5 is-small"
-        role="navigation"
-        aria-label="pagination"
-      >
-        <button
-          type="button"
-          className="pagination-previous"
-          disabled={page === 1}
-          onClick={() => setPage(page - 1)}
-        >
-          {PREVIOUS}
-        </button>
-        <button
-          type="button"
-          className="pagination-next"
-          disabled={data.length < DEFAULT_RANGE || !data.length}
-          onClick={() => setPage(page + 1)}
-        >
-          {NEXT}
-        </button>
-        <ul className="pagination-list">
-          {pages.map(pageNumber => (
-            <li key={pageNumber}>
-              <button
-                type="button"
-                className={cn('pagination-link', {
-                  'is-current': page === pageNumber
-                })}
-                onClick={() => setPage(pageNumber)}
-              >
-                {pageNumber}
-              </button>
-            </li>
-          ))}
-          <li>
+            </tbody>
+          </table>
+          <nav
+            className="pagination is-rounded mb-5 is-small"
+            role="navigation"
+            aria-label="pagination"
+          >
             <button
               type="button"
-              className="pagination-link"
-              disabled={data.length < DEFAULT_RANGE || !data.length}
-              onClick={() => setPage(pages.length + 1)}
+              className="pagination-previous"
+              disabled={page === 1}
+              onClick={() => setPage(page - 1)}
             >
-              ?
+              {PREVIOUS}
             </button>
-          </li>
-        </ul>
-      </nav>
-      {errorMessage ? (
-        <div className="notification is-danger">{errorMessage}</div>
-      ) : (
-        <></>
+            <button
+              type="button"
+              className="pagination-next"
+              disabled={data.length < DEFAULT_RANGE || !data.length}
+              onClick={() => setPage(page + 1)}
+            >
+              {NEXT}
+            </button>
+            <ul className="pagination-list">
+              {pages.map(pageNumber => (
+                <li key={pageNumber}>
+                  <button
+                    type="button"
+                    className={cn('pagination-link', {
+                      'is-current': page === pageNumber
+                    })}
+                    onClick={() => setPage(pageNumber)}
+                  >
+                    {pageNumber}
+                  </button>
+                </li>
+              ))}
+              <li>
+                <button
+                  type="button"
+                  className="pagination-link"
+                  disabled={data.length < DEFAULT_RANGE || !data.length}
+                  onClick={() => setPage(pages.length + 1)}
+                >
+                  ?
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </>
       )}
-      {loading ? (
-        <progress className="progress is-small is-primary" max="100" />
-      ) : (
-        <></>
-      )}
+      <div>
+        {errorMessage && (
+          <div className="notification is-danger">{errorMessage}</div>
+        )}
+      </div>
+      {loading && <progress className="progress is-small is-primary" max="100" />}
     </>
   );
 }
